474017f9ff8c86d2c326b797e4d8e2e28da565b4
Pritesh Zaver <priteshzaver@gmail.com>
2023-01-31 15:34:01 -0600

    display list of current users playlists and save songs to playlist

--- src/components/songs/Songs.js
+++ src/components/songs/Songs.js
@@ -1,4 +1,22 @@
-export const Songs = ({songName, songId, songArtist}) => {
+import { useEffect, useState } from "react"
+
+export const Songs = ({ songName, songId, songArtist }) => {
+    const [listPlaylists, setListPlaylists] = useState([])
+    const [playlistSongs, setPlaylistSongs] = useState({
+        songId: songId,
+        playlistId: 0
+    })
+    const localSpotifyUser = localStorage.getItem("spotify_user")
+    const spotifyUser = JSON.parse(localSpotifyUser)
+
+    useEffect(() => {
+        fetch(`http://localhost:8088/playlists`)
+            .then(response => response.json())
+            .then((data) => {
+                setListPlaylists(data)
+            })
+    }, [])
+
     const saveSong = () => {
         return <button
             onClick={() => {
@@ -8,24 +26,54 @@ export const Songs = ({songName, songId, songArtist}) => {
                         "Content-Type": "application/json",
                     },
                     body: JSON.stringify({
-                        songName: songName,
+                        playlistId: playlistSongs.playlistId,
                         songId: songId
                     })
                 })
-                .then(response => response.json())
-                .then(() => {
-                    alert(`Your song was successfully saved`)
-                })
+                    .then(response => response.json())
+                    .then(() => {
+                        alert(`Your song was successfully saved`)
+                    })
             }}>
-                Save Song
-            </button>
+            Save Song
+        </button>
     }
-    
+
     return (
-        <section>
-            <header>{songName}</header>
-            <div>{songArtist}</div>
-            <footer>{saveSong()}</footer>
-        </section>
+        <form>
+            <section>
+                <header>{songName}</header>
+                <div>{songArtist}</div>
+            </section>
+            <fieldset>
+                <div>Select playlist to save to:</div>
+                {listPlaylists.map(listPlaylist => {
+                    return <>
+                        {
+                            spotifyUser.id === listPlaylist.userId
+                                ? <>
+                                    <div key={listPlaylist.id} className="radio">
+                                        <input
+                                            type="radio"
+                                            value={listPlaylist.id}
+                                            checked={listPlaylist.id === playlistSongs.playlistId}
+                                            onChange={(event) => {
+                                                const copy = { ...playlistSongs }
+                                                copy.playlistId = parseInt(event.target.value)
+                                                setPlaylistSongs(copy)
+                                            }} />
+                                        {listPlaylist.playlistName}
+                                    </div>
+                                </>
+                                : ""
+                        }
+
+                    </>
+                })}
+            </fieldset>
+            <fieldset>
+                {saveSong()}
+            </fieldset>
+        </form>
     )
 }
