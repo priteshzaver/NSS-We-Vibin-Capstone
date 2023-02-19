064a79b2abc3fac4a752c5c3f525f7b4ebf31e1f
Pritesh Zaver <priteshzaver@gmail.com>
2023-02-19 17:02:16 -0600

    change musicplayer back to original

--- src/components/musicplayer/MusicPlayer.js
+++ src/components/musicplayer/MusicPlayer.js
@@ -1,47 +1,33 @@
-import { useEffect, useState } from "react"
-import SpotifyPlayer from "react-spotify-web-playback"
+import { useEffect, useState } from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
 
 export const MusicPlayer = ({ selectedTrackUri }) => {
-    const [play, setPlay] = useState(false)
-    const [selectedTrack, setSelectedTrack] = useState([])
-    let token = window.localStorage.getItem("token")
+  const [play, setPlay] = useState(false);
+  let token = window.localStorage.getItem("token");
 
-    useEffect(() => {
-        fetch(`http://localhost:8088/selectedTrack`)
-            .then(response => response.json())
-            .then(data => {
-                setSelectedTrack(data[0].selectedUri)
-            })
-    }, [])
+  useEffect(() => {
+    setPlay(true);
+  }, [selectedTrackUri]);
 
-    if (!token)
-        return null
+  if (!token) return null;
 
-    return (
-        <>
-        <SpotifyPlayer
-            token={token}
-            showSaveIcon
-            callback={state => !state.isPlaying && setPlay(false)}
-            play={play}
-            uris={selectedTrack ? selectedTrack : []}
-            styles={{
-                activeColor: "#fff",
-                bgColor: "#333",
-                color: "#fff",
-                loaderColor: "#fff",
-                sliderColor: "#1cb954",
-                trackArtistColor: "#ccc",
-                trackNameColor: "#fff",
-                height: "55px",
-            }}
-            />
-            <button
-                onClick={() => {
-                    setPlay(!play);
-                }}
-            >
-            </button>
-            </>
-    )
-}
+  return (
+    <SpotifyPlayer
+      token={token}
+      showSaveIcon
+      callback={(state) => !state.isPlaying && setPlay(false)}
+      play={play}
+      uris={selectedTrackUri ? selectedTrackUri : []}
+      styles={{
+        activeColor: "#fff",
+        bgColor: "#333",
+        color: "#fff",
+        loaderColor: "#fff",
+        sliderColor: "#1cb954",
+        trackArtistColor: "#ccc",
+        trackNameColor: "#fff",
+        height: "55px",
+      }}
+    />
+  );
+};
